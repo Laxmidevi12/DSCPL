e3c7b6df9ebfbaca90c3efc726369dcd76e10ff5
Laxmidevi12 <151540185+Laxmidevi12@users.noreply.github.com>
2025-07-07 20:49:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import os
 import openai
-client = openai.OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+OPENAI_API_KEY = "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"
+client = openai.OpenAI(api_key=OPENAI_API_KEY])
 
   
 st.set_page_config(page_title="DSCPL - Spiritual Chatbot", layout="centered")
